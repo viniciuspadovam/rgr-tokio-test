46148d13403ed4a29b3ed6ca3ae96698f4e18cf0
viniciuspadovam <padovam3373@gmail.com>
2025-03-22 15:17:43 -0300

    build(api): add dependencies and minor build changes

--- api-java/src/main/resources/data.sql
+++ api-java/src/main/resources/data.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE IF NOT EXISTS clients (
 	id INT NOT NULL PRIMARY KEY,
 	first_name VARCHAR(250) NOT NULL,
@@ -15,7 +14,5 @@ CREATE TABLE IF NOT EXISTS addresses (
 	postal_code VARCHAR(10),
 	city VARCHAR(250),
 	state VARCHAR(250),
-	country VARCHAR(250),
 	FOREIGN KEY (id_client) REFERENCES public.clients(id)
 );
-
